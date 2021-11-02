5a90dcbdba7cbf009312b5686635e3d46d1ae192
Adwiz <adwiz@suroviy.ru>
2021-11-02 05:45:19 +0300

    retry queues added

--- example.py
+++ example.py
@@ -1,14 +1,25 @@
-import celery
-from tasks import add, divide
+from tasks import multiply_roulette
+import time
 
 
-def main():
-    chain = celery.chain(add.s(17, 15), divide.s(10))
-    print(chain)
-    result = chain()
-    print(result.state)
-    print(result.get())
-    print(result.state)
+def main(number_of_jobs=10):
+    results = [
+        multiply_roulette.delay(10, 30)
+        for _ in range(number_of_jobs)
+    ]
+    time.sleep(0.5)
+    pending_tasks = []
+    for i, result in enumerate(results):
+        if result.status == 'SUCCESS':
+            print(f'{result.id} -&gt; {result.get()}')
+        else:
+            print(f'{result.id} is {result.state}')
+            pending_tasks.append(i)
+
+    print('Ждем застрявшие задачи')
+    for result in [results[i] for i in pending_tasks]:
+        print(f'{result.id}', end=' ')
+        print(f'-&gt; {result.get()}')
 
 
 if __name__ == '__main__':
--- tasks.py
+++ tasks.py
@@ -1,20 +1,29 @@
 import celery
+import random
 
 app = celery.Celery(
-    'example-chain',
+    'example-retry',
     brocker='redis://localhost',
     backend='redis://localhost',
     broker_url='redis://localhost:6379',
-    task_routes={'tasks.add': {'queue': 'add'}, 'tasks.divide': {'queue': 'divide'}},
+    # task_routes={'tasks.add': {'queue': 'add'}, 'tasks.divide': {'queue': 'divide'}},
 
 )
 
 
-@app.task
-def add(x, y):
-    return x + y
+class UnluckyException(Exception):
+    pass
 
 
-@app.task
-def divide(x, y):
-    return x / y
+@app.task(
+    bind=True,
+    retry_backoff=True,
+    retry_kwargs={'max_retries': 3}
+)
+def multiply_roulette(self, x, y):
+    try:
+        if random.random() == 6:
+            raise UnluckyException
+        return x * y
+    except UnluckyException as exc:
+        self.retry(exc=exc)
